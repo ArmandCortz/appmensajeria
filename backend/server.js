e66a85a3b6081e27620f7fe0736b26e3a21a80c6
ArmandCortz <armand1515.lc@gmail.com>
2022-11-12 11:10:34 -0600

    version 1.0

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ app.get('/',(req,res)=>{
     res.send("API is running")
 });
 
-app.get
+app.get('/api/chat',(req,res)=>{
+    res.send(chats);
+})
 
 app.listen(5000, console.log("server start on port 5000"));
 
