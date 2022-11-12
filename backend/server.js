97ef1b8f03d2d66dd2175b9c4da7f036e8b34d00
ArmandCortz <armand1515.lc@gmail.com>
2022-11-12 10:45:45 -0600

    version 1.0

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("API is running")
+});
+
+app.get
+
+app.listen(5000, console.log("server start on port 5000"));
+
